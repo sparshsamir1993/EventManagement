271c33ec423cc49081a7e3cc98835d8b7a6f2eae
Sparsh Samir <sparshsamir1993@gmail.com>
2019-08-09 18:56:12 -0400

    convertor class added

---
+++ EventManagement/PeopleUnder100.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EventManagement
+{
+    class PeopleUnder100 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            int people = 0;
+            if (int.TryParse(value.ToString(), out people) && people < 100)
+            {
+                return Brushes.Red;
+            }
+            else
+            {
+                return Brushes.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
